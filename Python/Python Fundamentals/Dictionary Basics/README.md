6838c5bca6dfd527d6dc9a8a001400fa0d6db80a
Kenny Pham <kennympham@gmail.com>
2018-12-11 17:05:07 -0800

    Add files via upload
    Relocated files

---
+++ Python/Python Fundamentals/Dictionary Basics/README.md
@@ -0,0 +1,1 @@
+There are two steps to this process, building a dictionary and then gathering all the data from it. Write a function that can take in and print out any dictionary keys and values.
---
+++ Python/Python Fundamentals/Dictionary Basics/dictionary_basics.py
@@ -0,0 +1,10 @@
+me = {
+    'name': 'Kenny',
+    'age': 31,
+    'country of birth': 'The United States',
+    'favorite language': 'Python'
+}
+def dic(dic_name):
+    print 'My nme is {}. I am {}, was born in {}, and my favorite langugae is {}'.format(me['name'], me['age'], me['country of birth'], me['favorite language'])
+dic(me)
+
