f2468eaa5dda115d1b9b977d6a174366f93d2f95
Kenny Pham <kennympham@gmail.com>
2019-01-03 01:08:55 -0800

    Delete models.py

--- Django/Django and AJAX/ajax_post/apps/notes/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.db import models
-
-class Note(models.Model):
-    note = models.TextField()
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
